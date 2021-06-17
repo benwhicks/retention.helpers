a3348cbe607c9c994abaadb04f18d66136c7376d
Ben Hicks <benwhicks@gmail.com>
2021-06-17 21:20:26 +1000

    added CSU colour scheme

---
+++ R/colours.R
@@ -0,0 +1,61 @@
+
+# Colours for plotting ----------------------------------------------------
+
+#' Light colour palette
+#' @export csu_colours_light
+csu_colours_light <- c(
+  "#577ec1",  # light blue
+  "#519775",  # light green
+  "#e9cdc9",  # peach
+  "#c8b8a1",  # light tan
+  "#ffffff"   # white
+)
+
+
+
+#' Dark colour palette
+#' @export csu_colours_dark
+csu_colours_dark <- c(
+  "#f0572b",  # orange
+  "#232845",  # dark blue
+  "#0d3b31",  # dark green
+  "#746858",  # dark tan
+  "#222222"  # black
+)
+
+
+#' CSU colour palette
+#'
+#' @source https://about.csu.edu.au/brand/about
+#' @export csu_colours
+csu_colours <- c(
+      "#f0572b",  # orange
+      "#232845",  # dark blue
+      "#519775",  # light green
+      "#c8b8a1",  # light tan
+      "#0d3b31",  # dark green
+      "#577ec1",  # light blue
+      "#746858",  # dark tan
+      "#e9cdc9",  # peach
+      "#222222",  # black
+      "#ffffff"   # white
+    )
+
+#' CSU colour palette
+#'
+#' @source https://about.csu.edu.au/brand/about
+#' @export csu_colours_paired
+csu_colours_paired <- c(
+      "#232845",  # dark blue
+      "#577ec1",  # light blue
+      "#0d3b31",  # dark green
+      "#519775",  # light green
+      "#f0572b",  # orange
+      "#e9cdc9",  # peach
+      "#746858",  # dark tan
+      "#c8b8a1",  # light tan
+      "#222222",  # black
+      "#ffffff"   # white
+    )
+
+# scales::show_col(csu_paired_colours)
