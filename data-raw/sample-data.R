3d05b785fe14e0eb1566e9903c20c4c114f5d3cf
benwhicks <benwhicks@gmail.com>
2023-01-04 13:06:40 +1100

    added sample data and draft vignette

---
+++ data-raw/sample-data.R
@@ -0,0 +1,15 @@
+## code to prepare `sample data` dataset goes here
+
+toy_student_course <-
+    readxl::read_excel('data-raw/sample_data.xlsx',
+                       sheet = 'enrolments')
+toy_enrolments <-
+    readxl::read_excel('data-raw/sample_data.xlsx',
+                       sheet = 'enrolments')
+toy_academic <-
+    readxl::read_excel('data-raw/sample_data.xlsx',
+                       sheet = 'enrolments')
+
+usethis::use_data(toy_student_course, overwrite = TRUE)
+usethis::use_data(toy_enrolments, overwrite = TRUE)
+usethis::use_data(toy_academic, overwrite = TRUE)
