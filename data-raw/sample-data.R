d7c869935d3c6a052671ec3d0c70e325e2da4b35
Hicks <bhicks@csu.edu.au>
2023-01-04 14:23:01 +1100

    vignette updates

--- data-raw/sample-data.R
+++ data-raw/sample-data.R
@@ -2,13 +2,13 @@
 
 toy_student_course <-
     readxl::read_excel('data-raw/sample_data.xlsx',
-                       sheet = 'enrolments')
+                       sheet = 'student_course')
 toy_enrolments <-
     readxl::read_excel('data-raw/sample_data.xlsx',
                        sheet = 'enrolments')
 toy_academic <-
     readxl::read_excel('data-raw/sample_data.xlsx',
-                       sheet = 'enrolments')
+                       sheet = 'academic')
 
 usethis::use_data(toy_student_course, overwrite = TRUE)
 usethis::use_data(toy_enrolments, overwrite = TRUE)
